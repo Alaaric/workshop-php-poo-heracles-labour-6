6a680f1d7c61e9adaa8d9bb04d6a7ffaeabfb110
Alaric Henrot <alaric.henrot@ospi.fr>
2024-08-01 11:27:02 +0200

    add equipable interface, shove class and add shovel to heracles inventory

--- src/Fighter/Hero.php
+++ src/Fighter/Hero.php
@@ -2,48 +2,29 @@
 
 namespace App\Fighter;
 
+use App\Equipable;
 use App\Inventory\Shield;
 use App\Inventory\Weapon;
+use App\Inventory\Shovel;
+
 use App\Movable;
 
 class Hero extends Fighter implements Movable
 {
-    private ?Weapon $weapon = null;
-    private ?Shield $shield = null;
     protected int $strength = 20;
     protected int $dexterity = 6;
     protected string $image = 'heracles.svg';
+    private ?Equipable $secondHand = null;
+    private ?Weapon $weapon = null;
+    private ?Shield $shield = null;
 
     public function getDamage(): int
     {
         $damage = $this->getStrength();
-        if($this->getWeapon() !== null) {
+        if ($this->getWeapon() !== null) {
             $damage += $this->getWeapon()->getDamage();
         }
         return $damage;
-    }
-
-    public function getDefense(): int
-    {
-        $defense = $this->getDexterity();
-        if ($this->getShield() !== null) {
-            $defense += $this->getShield()->getProtection();
-        }    
-
-        return $defense;
-    }
-
-    /**
-     * Get the value of range
-     */ 
-    public function getRange(): float
-    {
-        $range = $this->range;
-        if ($this->getWeapon() instanceof Weapon) {
-            $range += $this->getWeapon()->getRange();
-        }
-
-        return $range;
     }
 
     /**
@@ -63,6 +44,16 @@ class Hero extends Fighter implements Movable
         $this->weapon = $weapon;
     }
 
+    public function getDefense(): int
+    {
+        $defense = $this->getDexterity();
+        if ($this->getShield() !== null) {
+            $defense += $this->getShield()->getProtection();
+        }
+
+        return $defense;
+    }
+
     /**
      * Get the value of shield
      */
@@ -78,5 +69,29 @@ class Hero extends Fighter implements Movable
     public function setShield(?Shield $shield): void
     {
         $this->shield = $shield;
+    }
+
+    public function getSecondHand(): ?Equipable
+    {
+        return $this->secondHand;
+    }
+
+    public function setSecondHand(Equipable $equipable): void
+    {
+         $this->secondHand = $equipable;
+
+    }
+
+    /**
+     * Get the value of range
+     */
+    public function getRange(): float
+    {
+        $range = $this->range;
+        if ($this->getWeapon() instanceof Weapon) {
+            $range += $this->getWeapon()->getRange();
+        }
+
+        return $range;
     }
 }
